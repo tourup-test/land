0ed16652eaed048fa230c90dd1f3376a7cc17550
tourup-test <43450957+tourup-test@users.noreply.github.com>
2018-09-22 09:19:00 +0530

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -1,11 +1,5 @@
 <?php 
-$phn = preg_replace('/\(/is','',$_POST['phone_number']);
-$phn = preg_replace('/\)/is','',$phn);
-$phn = preg_replace('/-/is','',$phn);
-$phn = preg_replace('/\s+/is','',$phn);
-$ph =  '+'.$_POST['phoneCode'].''.$phn;
-
-$data ='{"phone_number":"'.$ph.'"}';
  
-echo $data;
+ 
+echo 'hi';
 ?>
