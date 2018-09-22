674fface9f145131e5cbc5de8f9719e3b1a40487
tourup-test <43450957+tourup-test@users.noreply.github.com>
2018-09-22 09:11:18 +0530

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -7,17 +7,5 @@ $ph =  '+'.$_POST['phoneCode'].''.$phn;
 
 $data ='{"phone_number":"'.$ph.'"}';
  
-
-$ch = curl_init('http://tourup-backend.us-west-1.elasticbeanstalk.com//beta_confirmation/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-// execute!
-$response = curl_exec($ch);
-
-// close the connection, release resources used
-curl_close($ch);
-
-// do anything you want with your response
-echo "Great! you're on the waitlist";
+echo $data;
 ?>
